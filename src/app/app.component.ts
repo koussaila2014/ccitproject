d7389e81d8ec70e7ad1c3b85a088508fdbe22d80
koussaila2014 <56189659+koussaila2014@users.noreply.github.com>
2020-05-04 18:38:39 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,14 @@ export class AppComponent {
   title = 'personal-website-template';
   constructor() {
     const firebaseConfig = {
-      apiKey: "AIzaSyB_bHNS4rUrKc0DjIFbGRn3vivEn06_VO8",
-      authDomain: "test-6f1ff.firebaseapp.com",
-      databaseURL: "https://test-6f1ff.firebaseio.com",
-      projectId: "test-6f1ff",
-      storageBucket: "test-6f1ff.appspot.com",
-      messagingSenderId: "1070631459889",
-      appId: "1:1070631459889:web:77891bb7f3227c390cc910",
-      measurementId: "G-QMXRGR6V1N"
+      apiKey: "xxxxx",
+      authDomain: "xxxxxxx",
+      databaseURL: "xxxxx",
+      projectId: "xxxxx",
+      storageBucket: "xxxxxx",
+      messagingSenderId: "xxxxxx",
+      appId: "xxxxxxx",
+      measurementId: "xxxxxx"
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
